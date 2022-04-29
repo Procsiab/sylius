98c5103879a7d349f299bad45288c305eb019abe
Lorenzo Prosseda <lerokamut@gmail.com>
2022-04-29 18:43:54 +0200

    Updated Containerfile, fixed env init and install

--- compose/init-env.sh
+++ compose/init-env.sh
@@ -1,7 +1,18 @@
 #! /usr/bin/env bash
+
+# Delete previous èersistent data
 sudo rm -rf app data
 mkdir app data
+# Change the owner of the data folder to mariadb UID
 podman unshare chown 999:999 data
-podman run -d --name sylius-app-copy-files sylius-app
+# Copy the application project from the container folder
+podman run -d --name sylius-app-copy-files docker.io/procsiab/sylius:v1.0-amd64
 podman cp sylius-app-copy-files:/var/www/html/app/. app/
 podman rm -f sylius-app-copy-files
+# Copy the default locale and currency declaration to the project
+cp ./locale_currency.yaml app/config/services.yaml
+# Use ACLs to allow www-data to write var and media
+podman unshare setfacl -dR -m u:82:rwX app/var
+podman unshare setfacl -R -m u:82:rwX app/var
+podman unshare setfacl -dR -m u:82:rwX app/public/media
+podman unshare setfacl -R -m u:82:rwX app/public/media
--- compose/run-install.sh
+++ compose/run-install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# Update Composer dependencies
+podman-compose exec app sh -c 'composer update --no-interaction'
 # Install the application AFTER connecting to the database
-podman-compose exec app sh -c 'php bin/console sylius:install --no-interaction'
+podman-compose exec app sh -c 'php bin/console sylius:install'
 # GitHub issue sylius/sylius:
 # https://github.com/Sylius/Sylius/issues/12893#issuecomment-898884792
 sed -i "s/import sass from 'gulp-sass';/var sass = require('gulp-sass')(require('sass'));/g" ./app/src/Sylius/Bundle/ShopBundle/gulpfile.babel.js
@@ -9,5 +11,3 @@ sed -i "s/import sass from 'gulp-sass';/var sass = require('gulp-sass')(require(
 podman-compose exec app sh -c 'apk add yarn python2 g++ && yarn add gulp-sass@5.0.0 && yarn add gulp-dart-sass@^1.0.2'
 # Build the graphical assets
 podman-compose exec app sh -c 'yarn install && yarn build'
-# Run again the installation setup to interactively create an admin account
-podman exec -it compose_app_1 sh -c 'php bin/console sylius:install:setup'
